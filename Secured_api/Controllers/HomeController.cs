46dbeb202361699f96358b2a57c26d08ea94aa32
$AMLRT-app <Mohamed.ragab@integrant.com>
2020-08-02 18:11:43 +0200

    Initial commit

---
+++ Secured_api/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Secured_api.Controllers
+{
+    [Route("")]
+    [ApiController]
+    [AllowAnonymous]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task< IActionResult> Get()
+        {
+            var cert = await HttpContext.Connection.GetClientCertificateAsync();
+            return Ok(new {
+                cert.Thumbprint,
+                cert.Subject,
+                cert.SubjectName,
+                cert.FriendlyName
+
+            });
+        }
+    }
+}
---
+++ Secured_api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Secured_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var cert = await HttpContext.Connection.GetClientCertificateAsync();
+
+            return Ok(new
+            {
+                cert.Thumbprint,
+                cert.Subject,
+                cert.FriendlyName
+            });
+           
+        }
+    }
+}
